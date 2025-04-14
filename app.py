5c4ecc6f34bfcd3cbc26127eff4c480f17a5255a
@fmota-dev <fmota.web@gmail.com>
2025-04-14 20:08:18 -0300

    att: código rondando no windows

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import time
-from scrapy.cmdline import execute
+import subprocess
 
 # Adiciona a pasta src ao path para que o pacote 'etl_olx' seja encontrado
 sys.path.insert(0, os.path.abspath("src"))
@@ -9,11 +9,31 @@ sys.path.insert(0, os.path.abspath("src"))
 # Define o módulo de configurações do Scrapy
 os.environ['SCRAPY_SETTINGS_MODULE'] = 'etl_olx.settings'
 
+# Caminho completo para o executável scrapy no ambiente virtual
+SCRAPY_EXECUTABLE = os.path.join(
+    os.path.abspath(".venv"), "Scripts", "scrapy.exe")
+
 contador = 1
 while True:
-    print(f"\nExecução {contador} iniciada...")
-    execute(["scrapy", "crawl", "olx_car"])
+    try:
+        print(f"\nExecução {contador} iniciada...")
+        # Define o PYTHONPATH para incluir o diretório raiz do projeto e o diretório src
+        env = os.environ.copy()
+        env["PYTHONPATH"] = f"{os.path.abspath('.')};{os.path.abspath('src')}"
+        # Adiciona o ambiente virtual ao PATH
+        env["PATH"] += f";{os.path.abspath('.venv/Scripts')}"
 
+        # Executa o comando scrapy crawl usando o caminho completo do executável
+        subprocess.run([SCRAPY_EXECUTABLE, "crawl", "olx_car"],
+                       check=True, env=env)
+    except subprocess.CalledProcessError as e:
+        # Captura erros do subprocesso
+        print(f"Erro ao executar o Scrapy: {e}")
+    except Exception as e:
+        # Captura outros erros
+        print(f"Erro inesperado: {e}")
+
+    # Aguarda 10 minutos antes da próxima execução
     tempo_espera = 600
     while tempo_espera > 0:
         minutos = tempo_espera // 60
